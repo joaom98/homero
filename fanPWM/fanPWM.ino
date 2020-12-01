d16c24888e7235f7354148decd3ad26d62f47cba
JoaoM <jm@pop-os-acer.localdomain>
2020-12-01 20:41:50 -0300

    Trocado o arquivo do FanPWM para apenas controlar o fan

--- fanPWM/fanPWM.ino
+++ fanPWM/fanPWM.ino
@@ -5,29 +5,7 @@
 #define PWM_PIN 9 // Fio Azul
 FanController fan(SENSOR_PIN, SENSOR_THRESHOLD, PWM_PIN);
 
-
-#include <PID_v1.h>
-#include <EduIntro.h>
-
-LM35 lm35(A0);  // creating the object 'lm35' on pin A0
-
-double C;
-
-//Define Variables we'll be connecting to
-double Desejado, Entrada, Saida;
-
-//Specify the links and initial tuning parameters
-PID myPID(&Entrada, &Saida, &Desejado, 100, 500, 1, REVERSE);
-
-
-double readTemperature (LM35 sensor, int samples) {
-  double soma = 0;
-   for ( int i=0 ; i < samples ; i++ ) {
-      soma += sensor.readCelsius(); 
-   }
-
-   return soma / samples;
-}
+#define VELOCIDADE 100 // Numero de 0 a 100 que define a velocidade do cooler
 
 void setup()
 {
@@ -41,33 +19,14 @@ void setup()
   Serial.print("Duty cycle: ");
   Serial.println(dutyCycle, DEC);
 
-
-  //initialize the variables we're linked to
-  Desejado = 10;
-
-  //tell the PID to range between 0 and the full window size
-  myPID.SetOutputLimits(0, 100);
-
-  //turn the PID on
-  myPID.SetMode(AUTOMATIC);
-
 }
 
 void loop()
 {
-  C = readTemperature(lm35, 200);       // Reading the temperature in Celsius degrees 
-  Entrada = C;
-  myPID.Compute();
+  
+  fan.setDutyCycle(VELOCIDADE);
 
-  // Set fan duty cycle
-  //fan.setDutyCycle(Saida);
-
-  fan.setDutyCycle(Saida);
-
-
-  Serial.print(C);
-  Serial.print("\t");
-  Serial.print(Saida);
+  Serial.print(VELOCIDADE);
   Serial.print("\t");
   Serial.print(fan.getSpeed());
   Serial.println("RPM");
